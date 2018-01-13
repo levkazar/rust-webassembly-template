27e53d607e9eebefaa4fb9b8b812f83af46db63a
Christian Wolff <christian.wolff@kofax.com>
2018-01-13 20:47:32 +0100

    Now changed to bundle.js, for real

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "package.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
   },
   module: {
